7e098ee808e7608e03d491f812dc309fbd3e0ca5
yap32 <131024539+yap32@users.noreply.github.com>
2023-09-18 01:22:00 +0800

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -79,6 +79,82 @@ namespace myImages {
 5 5 7 4 4 4 5 5 4 e e e e e . . 
 . 4 5 5 . . . . . . . 4 5 5 . . 
 `;
+            case "image6":
+            case "myImage4":return img`
+3 c 3 c 3 c 3 c 3 c 3 a . . . . 
+b c b c b c b c b c b b . . . . 
+a a a b a b a b a b b 8 . . . . 
+a 3 b b b b b b b b 3 8 . . . . 
+. 3 b b b b b b 3 3 b . . . . . 
+. b a b b b b a b b a c . . . . 
+. a a 3 b b b b a b b 5 3 3 3 3 
+. a a b b b b 8 a b 5 5 3 3 3 3 
+. b a a b b b b b b 8 8 . . . . 
+. 8 b b b b b b 8 8 8 . . . . . 
+d 8 8 8 8 8 8 8 8 8 8 a . . . . 
+4 8 4 8 4 8 4 8 4 8 4 4 . . . . 
+a 8 a 8 a 8 a 8 a 8 a 8 . . . . 
+a 8 a 8 a 8 a 8 a 8 a 8 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "myImage5":return img`
+. . a a 4 d . . . . . . a a b 3 
+. . 8 8 8 8 8 b a a b 3 3 a c c 
+. . a a 4 8 b a a a a b b a b 3 
+. . 8 8 8 8 b a b 3 b b b b c c 
+. . a a 4 8 b b b b b b b a b 3 
+. . 8 8 8 8 b b b b b b b b c c 
+. . a a 4 8 b b b b b b b a b 3 
+. . 8 8 8 8 b b 8 b a b b b c c 
+. . a a 4 8 8 b a a b 3 b a b 3 
+. . 8 8 8 8 8 b b b b 3 b b c c 
+. . a a 4 8 8 8 5 b a b 3 b b 3 
+. . 8 8 4 a . 8 5 5 c . 8 8 b a 
+. . . . . . . . 3 3 . . . . . . 
+. . . . . . . . 3 3 . . . . . . 
+. . . . . . . . 3 3 . . . . . . 
+. . . . . . . . 3 3 . . . . . . 
+`;
+            case "image5":
+            case "myImage3":return img`
+. . . . . . 3 3 . . . . . . . . 
+. . . . . . 3 3 . . . . . . . . 
+. . . . . . 3 3 . . . . . . . . 
+. . . . . . 3 3 . . . . . . . . 
+a b 8 8 . c 5 5 8 . a 4 8 8 . . 
+3 b b 3 b a b 5 8 8 8 4 a a . . 
+c c b b 3 b b b b 8 8 8 8 8 . . 
+3 b a b 3 b a a b 8 8 4 a a . . 
+c c b b b a b 8 b b 8 8 8 8 . . 
+3 b a b b b b b b b 8 4 a a . . 
+c c b b b b b b b b 8 8 8 8 . . 
+3 b a b b b b b b b 8 4 a a . . 
+c c b b b b 3 b a b 8 8 8 8 . . 
+3 b a b b a a a a b 8 4 a a . . 
+c c a 3 3 b a a b 8 8 8 8 8 . . 
+3 b a a . . . . . . d 4 a a . . 
+`;
+            case "image8":
+            case "myImage6":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 5 . . . . . . . 
+. . . . . . 5 1 1 5 . . . . . . 
+. . . . . . 5 1 1 5 . . . . . . 
+. . . . . . . 5 5 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
